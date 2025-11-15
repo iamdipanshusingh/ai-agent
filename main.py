e252514e8624d7fc49f7c3c07542a5f8fd27e53e
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2025-11-15 22:56:07 +0530

    added while true

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from src.agent import Agent
 agent_instance = Agent("https://iamdipanshus.in")
 agent = agent_instance.get_agent()
 
-query = input("Bot: hey, how may I help you?\n")
+print("Bot: hey, how may I help you?\n")
+while True:
+    query = input("You: ")
+    
+    if query.lower() in ["stop", "exit", "quit"]:
+        print("Adios...")
+        break
 
-for step in agent.stream(
-    {"messages": [{"role": "user", "content": query}]}, stream_mode="values"
-):
-    step["messages"][-1].pretty_print()
+    for step in agent.stream(
+        {"messages": [{"role": "user", "content": query}]}, stream_mode="values"
+    ):
+        step["messages"][-1].pretty_print()

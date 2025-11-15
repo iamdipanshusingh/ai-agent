611b26c7b67e0a843c409197f194af2eadf55f79
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2025-11-15 16:44:46 +0530

    chore: class based agent

--- src/retriever.py
+++ src/retriever.py
@@ -1,41 +1,34 @@
-import bs4
-from langchain.agents import AgentState, create_agent
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.messages import MessageLikeRepresentation
+from langchain.agents import create_agent
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.tools import tool
 
 from src import model
-from src.embeddings import vector_store, embeddings
+from src.embeddings import vector_store
 from src.loaders import web_loader
-
-# Load and chunk contents of the blog
-loader = web_loader("https://iamdipanshus.in")
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(docs)
-
-# Index chunks
-_ = vector_store.add_documents(documents=all_splits)
+from src.tools import retrieve_context
 
 
-# Construct a tool for retrieving context
-@tool(response_format="content_and_artifact")
-def retrieve_context(query: str):
-    """Retrieve information to help answer a query."""
-    retrieved_docs = vector_store.similarity_search(query, k=2)
-    serialized = "\n\n".join(
-        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
-        for doc in retrieved_docs
-    )
-    return serialized, retrieved_docs
+class Agent:
+    """Creates an agent with a prompt, to answer queries"""
+    def __init__(self):
+        # Load and chunk contents of the blog
+        loader = web_loader("https://iamdipanshus.in")
+        docs = loader.load()
 
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        all_splits = text_splitter.split_documents(docs)
 
-tools = [retrieve_context]
-# If desired, specify custom instructions
-prompt = (
-    "You have access to a tool that retrieves context from a blog post. "
-    "Use the tool to help answer user queries."
-)
-agent = create_agent(model, tools, system_prompt=prompt)
+        # Index chunks
+        _ = vector_store.add_documents(documents=all_splits)
+
+        tools = [retrieve_context]
+        # If desired, specify custom instructions
+        prompt = (
+            "You have access to a tool that retrieves context from a blog post. "
+            "Use the tool to help answer user queries."
+        )
+        self.agent = create_agent(model, tools, system_prompt=prompt)
+        
+    def get_agent(self):
+        return self.agent
